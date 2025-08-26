7952a507292f3c943c0931e649238838625d8620
Rob8624 <robert_melen@yahoo.co.uk>
2025-08-26 14:13:17 +0100

    added middlewar apart from Visitor

--- mysite/home/middleware.py
+++ mysite/home/middleware.py
@@ -6,7 +6,9 @@ import uuid
 
 
 class VisitorMiddleware(MiddlewareMixin):
-     pass
+    def process_request(self, request):
+       
+        return None
     
 
 
@@ -14,8 +16,58 @@ class VisitorMiddleware(MiddlewareMixin):
 
 
 class MyCookieProcessingMiddleware:
-   pass
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        # Will only add cookies if the request does not have them already
+        trigger = request.headers.get("HX-Trigger")
+
+        if trigger == "accept":
+            if not request.COOKIES.get('htmxaccepted'):
+                request.COOKIES['htmxaccepted'] = True
+
+        elif trigger == "decline":
+            print("decline trigger")
+            if not request.COOKIES.get('htmxdecline'):
+                response = HttpResponse()
+                response.set_cookie('htmxdecline', True)
+                return response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        return response
     
         
 class SimpleMiddleware:
-   pass
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+    
+    def __call__(self, request):
+         
+        
+         response = self.get_response(request)
+
+         trigger = request.headers.get("HX-Trigger")
+
+         if trigger == "accept":
+            print("accept trigger")
+            if not request.COOKIES.get('htmxaccepted'):
+                response.set_cookie('htmxacceptedmiddleware', True)
+                response["HX-Refresh"] = "true"
+
+         elif trigger == "decline":
+            print("decline trigger")
+            if not request.COOKIES.get('htmxdecline'):
+                 response.set_cookie('htmxdeclinedmiddleware', True)
+                
+                 response["HX-Refresh"] = "true"
+                 
+                 
+
+        # Code to be executed for each request/response after
+        # the view is called.
+        
+         return response
